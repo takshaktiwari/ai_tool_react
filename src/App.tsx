f7d292f2083c973c40c270156cc60b5b07e21a38
Aman Dubey <aman97642484@gmail.com>
2026-04-15 11:05:51 +0530

    error fixed

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,11 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter
+        basename={
+          import.meta.env.MODE === "production" ? "/ai_tool_react" : "/"
+        }
+      >
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="*" element={<NotFound />} />
